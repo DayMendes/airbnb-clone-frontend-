35d95be002d5cd000b1eaaaec68c7aad0c3d6523
arthurvergacas <arthurvdm@gmail.com>
2021-11-14 16:35:27 -0300

    Change index.d.ts to declaration.d.ts to tell typescript which declaration file is the correct
    Honestly don't quite understand why I had to do that, but the answer in SO is here: https://stackoverflow.com/questions/52759220/importing-images-in-typescript-react-cannot-find-module

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import logo from "../logo-512.png";
 export default function Navbar() {
     return (
         <nav>
-            <img src="" alt="" />
+            <img src={logo} alt="" />
         </nav>
     );
 }
