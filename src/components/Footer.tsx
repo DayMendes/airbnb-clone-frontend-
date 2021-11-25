baed142023e04491d2acf6c990f9ebe5d2eeb507
arthurvergacas <arthurvdm@gmail.com>
2021-11-25 11:05:15 -0300

    Add Nicolás and Luh names to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 import styles from "../styles/components/footer.module.css";
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <p>
-                Aplicação desenvolvida como desafio final do treinamento da{" "}
-                <a href="https://www.dbserver.com.br/">DB Server</a>.
-            </p>
-            <p>Esta aplicação foi desenvolvida em conjunto por:</p>
-            <p className={styles.autores}>
-                <a href="https://github.com/arthurvergacas">Arthur Vergaças</a>,{" "}
-                <a href="https://github.com/DayMendes">Dayane Mendes</a> e{" "}
-                <a href="https://github.com/roazambuja">Roberta Azambuja</a>.
-            </p>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <p>
+        Aplicação desenvolvida como desafio final do treinamento da <a href="https://www.dbserver.com.br/">DB Server</a>
+        .
+      </p>
+      <p>Esta aplicação foi desenvolvida em conjunto por:</p>
+      <p className={styles.autores}>
+        <a href="https://github.com/arthurvergacas">Arthur Vergaças</a>,{" "}
+        <a href="https://github.com/DayMendes">Dayane Mendes</a>, <a href="https://github.com/Luh-Santos">Luh Santos</a>
+        , <a href="https://github.com/Nicolasgbdas">Nicolás</a>, e{" "}
+        <a href="https://github.com/roazambuja">Roberta Azambuja</a>
+      </p>
+    </footer>
+  );
 }
