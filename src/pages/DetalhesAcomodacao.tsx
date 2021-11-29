f1820dc70c66b9c38d38dd755d9b1ded36acc93d
Dayane Mendes <day.neisca@gmail.com>
2021-11-29 15:02:44 -0300

    Alterações

--- src/pages/DetalhesAcomodacao.tsx
+++ src/pages/DetalhesAcomodacao.tsx
@@ -9,9 +9,7 @@ import "react-activity/dist/Spinner.css";
 import styles from "../styles/pages/detalhesAcomodacao.module.css";
 import { AppContext } from "../AppContext";
 
-
 export default function DetalhesAcomodacao() {
-
   let navigate = useNavigate();
 
   const { setMostrarCaixaDeBusca } = useContext(AppContext);
@@ -61,7 +59,9 @@ export default function DetalhesAcomodacao() {
       })
       .then((response) => {
         if (response.status === 200) {
-          setTextoReserva("O imóvel está disponível no período de seu interesse!");
+          setTextoReserva(
+            "O imóvel está disponível no período de seu interesse!",
+          );
           setDisponibilidade(true);
         }
       })
@@ -69,12 +69,14 @@ export default function DetalhesAcomodacao() {
         setDisponibilidade(false);
         if (error.response.status === 400) {
           setTextoReserva(
-            "Ops! Preencha todos os campos para verificarmos a disponibilidade do imóvel."
+            "Ops! Preencha todos os campos para verificarmos a disponibilidade do imóvel.",
           );
         } else if (error.response.status === 502) {
           setTextoReserva(error.response.data);
         } else {
-          setTextoReserva("Ocorreu algum erro durante a validação. Tente novamente!");
+          setTextoReserva(
+            "Ocorreu algum erro durante a validação. Tente novamente!",
+          );
         }
       });
   }
@@ -93,7 +95,7 @@ export default function DetalhesAcomodacao() {
         navigate("/reservado?referer=ok");
       })
       .catch((error) => {
-        if (error.response.status===401) {
+        if (error.response.status === 401) {
           navigate(`/login?id=${id}`);
         } else {
           alert("Ocorreu um erro ao realizar sua reserva!");
@@ -106,7 +108,11 @@ export default function DetalhesAcomodacao() {
       {acomodacao != null ? (
         <>
           <div className={styles.informacoes}>
-            <img className={styles.img} src={acomodacao.imagem} alt={acomodacao?.descricao} />
+            <img
+              className={styles.img}
+              src={acomodacao.imagem}
+              alt={acomodacao?.descricao}
+            />
             <h1>Informações sobre o local: </h1>
             <p>Tipo de acomodação: {acomodacao.categoria}</p>
             <p>Até {acomodacao.numeroDePessoas} pessoas</p>
@@ -115,8 +121,9 @@ export default function DetalhesAcomodacao() {
               {acomodacao.comodidades.quartos} quarto(s)
             </p>
             <p>
-              Regras: {acomodacao.regras.animais ? "permitido" : "proibido"} animais,{" "}
-              {acomodacao.regras.fumar ? "permitido" : "proibido"} fumar
+              Regras: {acomodacao.regras.animais ? "permitido" : "proibido"}{" "}
+              animais, {acomodacao.regras.fumar ? "permitido" : "proibido"}{" "}
+              fumar
             </p>
             <p>Valor: R$ {acomodacao.preco}</p>
           </div>
@@ -136,7 +143,9 @@ export default function DetalhesAcomodacao() {
                       type="date"
                       id="check-in"
                       name="check-in"
-                      onChange={(event) => setDataInicio(new Date(event.target.value))}
+                      onChange={(event) =>
+                        setDataInicio(new Date(event.target.value))
+                      }
                       placeholder="Check In"
                     />
                   </div>
@@ -150,7 +159,9 @@ export default function DetalhesAcomodacao() {
                       type="date"
                       id="check-out"
                       name="check-out"
-                      onChange={(event) => setDataTermino(new Date(event.target.value))}
+                      onChange={(event) =>
+                        setDataTermino(new Date(event.target.value))
+                      }
                       placeholder="Check Out"
                     />
                   </div>
@@ -178,10 +189,14 @@ export default function DetalhesAcomodacao() {
                     <p>
                       {" "}
                       Valor total: R${" "}
-                      {((dataTermino!.getTime() - dataInicio!.getTime()) / (24 * 60 * 60 * 1000)) *
+                      {((dataTermino!.getTime() - dataInicio!.getTime()) /
+                        (24 * 60 * 60 * 1000)) *
                         acomodacao!.preco}
                     </p>
-                    <button onClick={reservar} className={styles.buttonReservar}>
+                    <button
+                      onClick={reservar}
+                      className={styles.buttonReservar}
+                    >
                       Realizar reserva
                     </button>
                     <button
@@ -206,7 +221,13 @@ export default function DetalhesAcomodacao() {
           </div>
         </>
       ) : (
-        <>{requisicaoFeita ? <h1>Ops! Algo de errado aconteceu.</h1> : <Spinner />}</>
+        <>
+          {requisicaoFeita ? (
+            <h1>Ops! Algo de errado aconteceu.</h1>
+          ) : (
+            <Spinner />
+          )}
+        </>
       )}
     </section>
   );
